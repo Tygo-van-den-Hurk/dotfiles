99c08f9c3bfe4a3be9827103bcbfaee0b5428bd3
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2024-05-11 12:06:45 +0200

    Updated the restow command as it didn't really work well if you weren't in the home directory when you called it.

--- restow.sh
+++ restow.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+current_script_path="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-# Get the directory of the current script
-current_script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ncolors=$(tput colors) # if the terminal supports color, print in color, otherwise don't
+if [[ -n "$ncolors" && "$ncolors" -gt 2 ]]; then
+    echo -e "dotfiles are in \033[1;34m$current_script_path/\033[0m, stowing those to \033[1;34m$HOME/\033[0m."
+else
+    echo "dotfiles are in $current_script_path, stowing those to $HOME."
+fi
 
-# Use stow to manage symbolic links
-stow --dir="$current_script_path" --target="$HOME" .
+stow --dir="$current_script_path" --target="$HOME" --verbose=5 .
