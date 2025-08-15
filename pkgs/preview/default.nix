1cf83ad6c295225a36899cf0ce887a7d82e7aee1
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2025-08-15 12:48:08 +0200

    feat: Added shell completions
    This makes using it really convenient.

--- pkgs/preview/default.nix
+++ pkgs/preview/default.nix
@@ -1,58 +1,95 @@
 pkgs:
 with pkgs;
+
 let
-  script = builtins.readFile ./script.sh;
-  dependencies = ''
-    # Makes sure that the dependencies are installed and preferred.
 
-    function chafa() {
-      ${chafa}/bin/chafa "$@"
-      return $?
-    }
+  program = let
 
-    function bat() {
-      ${bat}/bin/bat "$@"
-      return $?
-    }
+    script = builtins.readFile ./script.bash;
 
-    function eza() {
-      ${eza}/bin/eza "$@"
-      return $?
-    }
+    dependencies = ''
+      # Makes sure that the dependencies are installed and preferred.
 
-    function basename() {
-      ${toybox}/bin/basename "$@"
-      return $?
-    }
+      function chafa() {
+        ${chafa}/bin/chafa "$@"
+        return $?
+      }
 
-    function file() {
-      ${file}/bin/file "$@"
-      return $?
-    }
+      function bat() {
+        ${bat}/bin/bat "$@"
+        return $?
+      }
 
-    function pdftoppm() {
-      ${poppler-utils}/bin/pdftoppm "$@"
-      return $?
-    }
+      function eza() {
+        ${eza}/bin/eza "$@"
+        return $?
+      }
 
-    function grep() {
-      ${toybox}/bin/grep "$@"
-      return $?
-    }
+      function basename() {
+        ${toybox}/bin/basename "$@"
+        return $?
+      }
 
-    function rm() {
-      ${toybox}/bin/rm "$@"
-      return $?
-    }
+      function file() {
+        ${file}/bin/file "$@"
+        return $?
+      }
 
-    function echo() {
-      ${toybox}/bin/echo "$@"
-      return $?
-    }
+      function pdftoppm() {
+        ${poppler-utils}/bin/pdftoppm "$@"
+        return $?
+      }
 
+      function grep() {
+        ${toybox}/bin/grep "$@"
+        return $?
+      }
+
+      function rm() {
+        ${toybox}/bin/rm "$@"
+        return $?
+      }
+
+      function echo() {
+        ${toybox}/bin/echo "$@"
+        return $?
+      }
+
+    '';
+  in
+  writeShellScriptBin "preview-script" ''
+    ${dependencies}
+    ${script}
   '';
 in
-writeShellScriptBin "preview" ''
-  ${dependencies}
-  ${script}
-''
+
+stdenv.mkDerivation rec {
+  name = "preview";
+  src = ./.;
+
+  inherit program;
+
+  buildPhase = ''
+    runHook preBuild
+    # ...
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir --parents $out/bin
+    cp $program/bin/preview-script $out/bin/preview
+
+    mkdir --parents $out/share/bash-completion/completions
+    cp $src/completions.bash $out/share/bash-completion/completions/preview.bash
+
+    # mkdir --parents $out/share/fish/vendor_completions.d/
+    # cp $src/completions.fish $out/share/fish/vendor_completions.d/preview.fish
+
+    # mkdir --parents $out/share/zsh/site-functions/
+    # cp $src/completions.zsh $out/share/zsh/site-functions/preview
+
+    runHook postInstall
+  '';
+}
