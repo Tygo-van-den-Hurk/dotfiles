67cb489064cc96fd85e7476812b9220c5eaab054
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2025-10-10 16:39:29 +0200

    chore(fmt): added `shfmt` and `shellcheck` formatters
    These are a formatter and linter for for `.bash`, `.sh`, `.zsh` and `.fish`
    respectively. They ensure the quality of shell scripts.

--- .config/treefmt.nix
+++ .config/treefmt.nix
@@ -6,5 +6,7 @@ _: {
     statix.enable = true; # nix static analysis
     deadnix.enable = true; # find dead nix code
     mdformat.enable = true; # markdown formatting
+    shfmt.enable = true; # Shell script formatting
+    shellcheck.enable = true; # Shell script linting
   };
 }
