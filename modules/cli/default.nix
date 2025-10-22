dda9f809df07991d2b7667d49d37223caeed4649
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2025-10-22 15:58:39 +0200

    feat: added global CLI config and add undollar package
    This allows for adding regular packages, and environment variables.
    Undollar strips the dollar sign from the beginning of the terminal command you
    just copied from StackOverflow when you were searching for what arguments to
    pass to tar (xzf? xvfJ? Or was it xvf? You never seem to remember).

--- modules/cli/default.nix
+++ modules/cli/default.nix
@@ -1,10 +1,13 @@
 {
+  config,
   lib,
+  pkgs,
   ...
 }:
 let
   inherit (lib) mkOption;
   inherit (lib) types;
+  inherit (lib) mkIf;
 
   inherit (types) bool;
 
@@ -15,6 +18,13 @@ in
     description = "Whether to enable CLI applications and terminal based tools.";
     default = true;
     type = bool;
+  };
+
+  # Global config that does not depend on anything.
+  config.home = mkIf config.${type}.enable {
+    packages = with pkgs; [
+      undollar
+    ];
   };
 
   imports = [
